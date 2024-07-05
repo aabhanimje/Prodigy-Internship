85a0c1f51e5bcbab4aa58e71d51927ee6d60dca6
aabhanimje <138747217+aabhanimje@users.noreply.github.com>
2024-07-05 23:18:06 +0530

    Add files via upload
    This project is first project where we have to design responsive landing page using HTML,CSS,JS.

---
+++ PRODIGY_WD_01/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+   
+  var button = document.getElementById("myBtn");
+  button.addEventListener("click", function() {
+      alert("Thanks for Choosing us.");
+  });
+});
